0cb485ca5dbbf51428eb4d0ca6b0ff6b091f8a7a
tenghooi <chantenghooi@gmail.com>
2021-08-26 04:00:31 +0800

    add CMakeLists.txt and solve intellisense problem

--- src/pcd_read.cpp
+++ src/pcd_read.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/kdtree/flann.h>
 
                    
 int main(int argc, const char** argv)
 {           
-          
+   std::cout << "Hello World" << std::endl;
    return 0;
-}
+} 
