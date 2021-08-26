57019f4e677edd9ff09537b93ca106bc66a02f9c
tenghooi <chantenghooi@gmail.com>
2021-08-27 01:45:02 +0800

    test pcl lib in pcd_read

--- src/pcd_read.cpp
+++ src/pcd_read.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <pcl/io/pcd_io.h>
-#include <pcl/kdtree/flann.h>
+#include <pcl/kdtree/flann.h>              
 
                    
 int main(int argc, const char** argv)
-{           
+{    
+
    std::cout << "Hello World" << std::endl;
    return 0;
 } 
